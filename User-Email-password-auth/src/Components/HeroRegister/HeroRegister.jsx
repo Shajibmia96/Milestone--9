2bbedf4e42d33f40d52e194721bd8e8a0ce94abe
Shajib96 <shajibahamedornob@gmail.com>
2024-05-26 22:10:26 +0600

    Login User And Accept Terms And Conditions

--- User-Email-password-auth/src/Components/HeroRegister/HeroRegister.jsx
+++ User-Email-password-auth/src/Components/HeroRegister/HeroRegister.jsx
@@ -15,7 +15,7 @@ const HeroRegister = () => {
         const Last_name = e.target.floating_last_name.value;
         const Phone  = e.target.floating_phone.value;
         const Company = e.target.floating_company.value;
-
+        const accepts = e.target.terms.checked;
         setUserRegister('')
         setSuccess('')
 
@@ -27,7 +27,15 @@ const HeroRegister = () => {
            setUserRegister("You password should have at least a Upper case character")
            return ;
          }
-        console.log(Email , Password , Repeat_password , First_name , Last_name, Phone , Company)
+         else if (Password !== Repeat_password){
+           setUserRegister("Your password do not match")
+           return;
+         }
+         else if (!accepts){
+            setUserRegister("Please accept our terms and conditions")
+            return;
+         }
+        console.log(Email , Password , Repeat_password , First_name , Last_name, Phone , Company , accepts)
         
         createUserWithEmailAndPassword(auth , Email , Password, Repeat_password, First_name,Last_name,Phone,Company)
         .then( result =>{
@@ -50,11 +58,14 @@ const HeroRegister = () => {
       <label name="email" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email address</label>
   </div>
   <div className="relative z-0 w-full mb-5 group">
+      <div className="relative">
       <input 
       type={showPassword ? "text":"password"} 
       name="floating_password" id="floating_password" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" "  required /> 
+      <span className="absolute top-3 right-5" onClick={()=>setShowPassword(!showPassword)}>{showPassword ? <IoMdEye /> : <IoMdEyeOff />}</span>
+      </div>
       <label name="password" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Password</label>
-      <span onClick={()=>setShowPassword(!showPassword)}>{showPassword ? <IoMdEye /> : <IoMdEyeOff />}</span>
+      
   </div>
   <div className="relative z-0 w-full mb-5 group">
       <input type={showPassword ? "text":"password"} name="repeat_password" id="floating_repeat_password" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
@@ -79,7 +90,12 @@ const HeroRegister = () => {
         <input type="text" name="floating_company" id="floating_company" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
         <label name="company" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Company (Ex. Google)</label>
     </div>
+    
   </div>
+  <div className="mb-2">
+        <input type="checkbox" name="terms" id="terms" />
+        <label className="ml-4" htmlFor="terms">Accept our terms and conditions</label>
+     </div>
   <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Register</button>
 </form>
 
--- User-Email-password-auth/src/Components/SingUp/SingUp.jsx
+++ User-Email-password-auth/src/Components/SingUp/SingUp.jsx
@@ -1,9 +1,87 @@
+import { signInWithEmailAndPassword } from "firebase/auth";
+import auth from "../../FireBase/FireBase.init";
+import { useState } from "react";
+import { IoMdEye, IoMdEyeOff } from "react-icons/io";
+
 const SingUp = () => {
-    return (
-        <div>
-              <h2 className="text-3 text-green-400">Please Sing Up</h2>
+      
+        const [singUpError, setSingUpError] = useState('')
+        const [singUpSuccess, setSingUpSuccess] = useState('')
+        const [showPassword , setShowPassword] = useState(false)
+
+
+       const handleSingUp = e =>{
+          e.preventDefault()
+          const email = e.target.email.value;
+          const password = e.target.password.value;
+          console.log(email , password)
+          setSingUpError("")
+          setSingUpSuccess("")
+          signInWithEmailAndPassword(auth , email , password)
+          .then(result =>{
+            const user = result.user
+            console.log(user)
+            setSingUpSuccess("Sing up successFull")
+          })
+          .catch(error =>{
+            const errorMessage = error.message;
+            setSingUpError(errorMessage)
+          })
+       }
+  return (
+    <div>
+      
+        <div className="hero mt-10 ">
+          <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-400">
+            <form onSubmit={handleSingUp} className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="email"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <div className="relative">
+                <input
+                  type={showPassword ? "text":"password"}
+                  name="password"
+                  placeholder="password"
+                  className="input input-bordered"
+                  required
+                />
+                 <span className="absolute top-3 right-35" onClick={()=>setShowPassword(!showPassword)}>{showPassword ? <IoMdEye /> : <IoMdEyeOff />}</span>
+                </div>
+                <label className="label">
+                  <a href="#" className="label-text-alt link link-hover">
+                    Forgot password?
+                  </a>
+                </label>
+              </div>
+              <div className="form-control mt-6">
+                <button className="btn btn-primary">Login</button>
+              </div>
+            </form>
+          </div>
         </div>
-    );
+
+        {
+            singUpError && <p className="text-red-400 text-center text-2xl mt-4">{singUpError}</p>
+        }
+        {
+            singUpSuccess && <p className="text-green-400-400 text-center text-2xl mt-4">{singUpSuccess}</p>
+        }
+     
+    </div>
+  );
 };
 
 export default SingUp;
