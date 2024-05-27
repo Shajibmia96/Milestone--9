dc65bf9bcc7a9bfb45e805f24c5178f17a89d323
Shajib96 <shajibahamedornob@gmail.com>
2024-05-27 19:31:27 +0600

    Create User Context For Shared Authentication

--- React-Auth-integration-Privet-route/src/Component/Home/Home.jsx
+++ React-Auth-integration-Privet-route/src/Component/Home/Home.jsx
@@ -1,7 +1,17 @@
+import { useContext } from "react";
+import { AuthContext } from "../../Provider/AuthProvider";
+
 const Home = () => {
+
+      const authInfo = useContext(AuthContext)
+      console.log(authInfo)
+      const {name, age, degree} = authInfo
     return (
-        <div>
+        <div className="text-center space-y-4 mt-5">
             <h1>This is Home component</h1>
+            <h1>User name : {name}</h1>
+            <p>User Age : {age}</p>
+            <p>User Degree : {degree}</p>
         </div>
     );
 };
